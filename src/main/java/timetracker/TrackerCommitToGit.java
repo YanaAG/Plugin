c0d78297b232ba92a5e9335ea830be6d393ddd66
яна <AvramenkoYana@yandex.ru>
2019-01-09 20:18:26 +0430

    Write new commits per session to file

--- src/main/java/timetracker/TrackerCommitToGit.java
+++ src/main/java/timetracker/TrackerCommitToGit.java
@@ -1,5 +1,6 @@
 package timetracker;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
@@ -10,6 +11,8 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Vector;
 
 public class TrackerCommitToGit {
     private String wayToProject;
@@ -18,6 +21,7 @@ public class TrackerCommitToGit {
     private FileRepositoryBuilder fileRepositoryBuilder = new FileRepositoryBuilder();
     private Repository repository;
     private Git git;
+    private Vector<Triple<String, String, String>> allCommits = new Vector<>();
 
     public TrackerCommitToGit(String way){
         wayToProject = way;
@@ -47,23 +51,51 @@ public class TrackerCommitToGit {
 
     public void setBeforeSession(){
         beforeSession = countCommitToGit();
-        try {
-            writer = new FileWriter("D:/way.txt", true);
-            writer.write("Before session " + beforeSession + "\r\n");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        allCommits.clear();
+        commitBeforeSession();
     }
 
     public void countAfterSession(){
-        int afterSession = countCommitToGit();
+        int afterSession = countCommitToGit(), diff;
+        if (afterSession != beforeSession) {
+            diff = afterSession - beforeSession;
+            try {
+                writer = new FileWriter("D:/way.txt", true);
+                writer.write("After session: \r\n");
+                Iterable<RevCommit> log = git.log().call();
+                for (RevCommit commit : log){
+                    if (diff != 0){
+                        writer.write("Commit: " + commit.getName() + "\r\n");
+                        writer.write("Commit message: " + commit.getFullMessage() + "\r\n");
+                        writer.write("Commit time: " + new Date(commit.getCommitTime() * 1000L)  + "\r\n" + "\r\n");
+                        diff--;
+                    }
+                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NoHeadException e) {
+                e.printStackTrace();
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void commitBeforeSession(){
+        Integer time;
+        String name, message;
         try {
-            writer = new FileWriter("D:/way.txt", true);
-            writer.write("After session " + afterSession + "\r\n");
-            writer.close();
-        } catch (IOException e) {
+            Iterable<RevCommit> log = git.log().call();
+            for (RevCommit commit : log){
+                name = commit.getName();
+                message = commit.getFullMessage();
+                time = commit.getCommitTime();
+                allCommits.add(Triple.of(name, message, time.toString()));
+            }
+        } catch (GitAPIException e) {
             e.printStackTrace();
         }
+        git.close();
     }
 }
